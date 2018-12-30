0df4ac08e0052d2142b251babce6d2960a052ea8
philipborg <philip.borgstrom@gmail.com>
2018-12-30 17:49:35 +0100

    Update to .NET Core 2.2

--- Server/WebApi/Startup.cs
+++ Server/WebApi/Startup.cs
@@ -16,7 +16,7 @@ namespace ReactTemplate.WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddSwaggerGen(c =>
             {
