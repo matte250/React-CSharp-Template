361371a381888998a7b9a8f3564f788d9fedae54
philipborg <philip.borgstrom@gmail.com>
2018-12-30 21:15:40 +0100

    Add configuration to Startup

--- Server/WebApi/Startup.cs
+++ Server/WebApi/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ReactTemplate.Data.Contexts;
 using ReactTemplate.Data.Data;
@@ -15,6 +16,13 @@ namespace ReactTemplate.WebApi
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
