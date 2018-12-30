80692e132a49f5a585d52962ca8b9b8e6c498972
philipborg <philip.borgstrom@gmail.com>
2018-12-30 18:06:07 +0100

    Add Yarn install

--- Server/WebApi/BuildScripts/ReactBuild.ps1
+++ Server/WebApi/BuildScripts/ReactBuild.ps1
@@ -1,4 +1,5 @@
 ﻿cd ../../Client/template-app
+yarn install
 yarn build
 Remove-Item "../../Server/WebApi/wwwroot/Generated" -Recurse
 Copy-Item "./build" -Destination "../../Server/WebApi/wwwroot/Generated" -Recurse
